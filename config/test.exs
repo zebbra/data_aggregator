a098eb23a082ed2124ac8158be4d817824fbcbd1
claudios <claudio.siegenthaler@zebbra.ch>
2023-08-06 13:26:11 +0200

    fixing ci tests

--- config/test.exs
+++ config/test.exs
@@ -8,11 +8,11 @@ import Config
 config :data_aggregator, DataAggregator.Repo,
   # username: "postgres",
   # password: "postgres",
-  port: 5432,
-  hostname: "localhost",
-  database: "data_aggregator_test#{System.get_env("MIX_TEST_PARTITION")}",
+  # port: 5432,
+  # hostname: "localhost",
+  # database: "data_aggregator_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
-  pool_size: 10
+  # pool_size: 10
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
