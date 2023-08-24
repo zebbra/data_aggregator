34e170e8292944f694b5b72118dd6696499e9739
claudios <claudio.siegenthaler@zebbra.ch>
2023-08-24 20:19:25 +0200

    fix linting

--- config/prod.exs
+++ config/prod.exs
@@ -8,8 +8,8 @@ import Config
 config :data_aggregator, DataAggregatorWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   check_origin: false,
-  code_reloader: false
-  server: true,
+  code_reloader: false,
+  server: true
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: DataAggregator.Finch
