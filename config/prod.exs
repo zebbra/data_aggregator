5cb0f4f8ed7bb8e0fbd8be89b073484fec692c5c
claudios <claudio.siegenthaler@zebbra.ch>
2023-08-24 20:19:11 +0200

    fix linter

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :data_aggregator, DataAggregatorWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   check_origin: false,
   code_reloader: false
   server: true,
