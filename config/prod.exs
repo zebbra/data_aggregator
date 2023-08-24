9db7d917ff86006e1bb8b1c38618690c8152db51
claudios <claudio.siegenthaler@zebbra.ch>
2023-08-24 20:15:10 +0200

    dev and test env config; phoenix endpoint config for staging system

--- config/prod.exs
+++ config/prod.exs
@@ -7,6 +7,9 @@ import Config
 # before starting your production server.
 config :data_aggregator, DataAggregatorWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
+  check_origin: false,
+  code_reloader: false
+  server: true,
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: DataAggregator.Finch
