4bc5fea2e1e2a69e20889da6cc978e77b667e12c
Hannes Wüthrich <hannes.wuethrich@zebbra.ch>
2023-10-30 14:43:15 +0100

    Add module doc

--- lib/data_aggregator/files/files.ex
+++ lib/data_aggregator/files/files.ex
@@ -1,4 +1,8 @@
 defmodule DataAggregator.Files do
+  @moduledoc """
+  This context provides an `DataAggregator.Files.Attachment` resource for managing files.
+  """
+
   use Ash.Api
 
   resources do
