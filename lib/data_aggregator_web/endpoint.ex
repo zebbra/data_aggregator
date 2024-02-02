cddb2e3011e28fcfa576e53e0080da13b74555c9
Michael Bärtschi <michael.baertschi@zebbra.ch>
2024-02-02 11:11:03 +0100

    chore(deps): updated live_view and phoenix

--- lib/data_aggregator_web/endpoint.ex
+++ lib/data_aggregator_web/endpoint.ex
@@ -5,7 +5,7 @@ defmodule DataAggregatorWeb.Endpoint do
   require Logger
 
   # add /health endpoint for liveness probes
-  plug DataAggregatorWeb.Plug.Health, path: "/health"
+  plug(DataAggregatorWeb.Plug.Health, path: "/health")
 
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
@@ -17,50 +17,54 @@ defmodule DataAggregatorWeb.Endpoint do
     same_site: "Lax"
   ]
 
-  socket "/live", Phoenix.LiveView.Socket,
+  socket("/live", Phoenix.LiveView.Socket,
     websocket: [connect_info: [session: @session_options]],
     longpoll: [connect_info: [session: @session_options]]
+  )
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
-  plug Plug.Static,
+  plug(Plug.Static,
     at: "/",
     from: :data_aggregator,
     gzip: false,
     only: DataAggregatorWeb.static_paths()
+  )
 
   # Serve waffle files in development
   if serve_files_from = Application.compile_env(:data_aggregator, :serve_files_from) do
     Logger.info("Serving files from #{serve_files_from}")
-    plug Plug.Static, at: "/files", from: serve_files_from, gzip: false
+    plug(Plug.Static, at: "/files", from: serve_files_from, gzip: false)
   end
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
-    plug Phoenix.LiveReloader
-    plug Phoenix.CodeReloader
-    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :data_aggregator
+    socket("/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket)
+    plug(Phoenix.LiveReloader)
+    plug(Phoenix.CodeReloader)
+    plug(Phoenix.Ecto.CheckRepoStatus, otp_app: :data_aggregator)
   end
 
-  plug Phoenix.LiveDashboard.RequestLogger,
+  plug(Phoenix.LiveDashboard.RequestLogger,
     param_key: "request_logger",
     cookie_key: "request_logger"
+  )
 
-  plug Plug.RequestId
-  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
+  plug(Plug.RequestId)
+  plug(Plug.Telemetry, event_prefix: [:phoenix, :endpoint])
 
-  plug Plug.Parsers,
+  plug(Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
+  )
 
-  plug Sentry.PlugContext
-  plug Plug.MethodOverride
-  plug Plug.Head
-  plug Plug.Session, @session_options
-  plug DataAggregatorWeb.Router
+  plug(Sentry.PlugContext)
+  plug(Plug.MethodOverride)
+  plug(Plug.Head)
+  plug(Plug.Session, @session_options)
+  plug(DataAggregatorWeb.Router)
 end
