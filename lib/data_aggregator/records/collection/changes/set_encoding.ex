0fe4a40e58ea7372fca4cba89bbbb67708ca8798
Michael Bärtschi <michael.baertschi@zebbra.ch>
2024-06-14 15:27:12 +0200

    chore(docs): update

--- lib/data_aggregator/records/collection/changes/set_encoding.ex
+++ lib/data_aggregator/records/collection/changes/set_encoding.ex
@@ -2,12 +2,12 @@ defmodule DataAggregator.Records.Collection.Changes.SetEncoding do
   @moduledoc """
   Manually track the encoding status of a collection.
 
-  You can subscribe to the `:set_encoding` and `:set_encoding_done` events
+  You can subscribe to the `:set_encoding` and `:set_idle_encoding` events
   to get notified when the encoding is started and finished.
 
   This change will start a polling process to check the encoding status
   of the collections records_count_encoding counter and update the collection
-  (call the `:set_encoding_done` update) once the encoding is done.
+  (call the `:set_idle_encoding` update) once the encoding is done.
   """
 
   use Ash.Resource.Change
