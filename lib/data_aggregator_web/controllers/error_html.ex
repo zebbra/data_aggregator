ea354d6b1e6c158d376502b6c4c96bfd286423b3
Michael Bärtschi <michael.baertschi@zebbra.ch>
2024-06-27 10:23:24 +0200

    feat(error_handling): added 404 and 500 templates and ignore not_found and status < 500 sentry errors

--- lib/data_aggregator_web/controllers/error_html.ex
+++ lib/data_aggregator_web/controllers/error_html.ex
@@ -13,7 +13,7 @@ defmodule DataAggregatorWeb.ErrorHTML do
   #   * lib/data_aggregator_web/controllers/error_html/404.html.heex
   #   * lib/data_aggregator_web/controllers/error_html/500.html.heex
   #
-  # embed_templates "error_html/*"
+  embed_templates "error_html/*"
 
   # The default is to render a plain text page based on
   # the template name. For example, "404.html" becomes
