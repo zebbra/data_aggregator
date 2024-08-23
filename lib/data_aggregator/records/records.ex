125359c76f9329e2cd2fe05ba589204c3853b960
Michael Bärtschi <michael.baertschi@zebbra.ch>
2024-08-23 14:43:01 +0200

    fix(jobs): max concurrency 12

--- lib/data_aggregator/records/records.ex
+++ lib/data_aggregator/records/records.ex
@@ -64,7 +64,9 @@ defmodule DataAggregator.Records do
 
   def import_max_concurrency do
     num_cpus = :erlang.system_info(:logical_processors_available)
-    get_env(:import_max_concurrency, num_cpus)
+    max_concurrency = get_env(:import_max_concurrency, num_cpus)
+
+    if max_concurrency > 12, do: 12, else: max_concurrency
   end
 
   def encode_timeout, do: get_env(:import_timeout)
