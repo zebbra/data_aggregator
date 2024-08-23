d42685ce374f66f4376d868a45d35ed57d50e5a4
Michael Bärtschi <michael.baertschi@zebbra.ch>
2024-08-23 12:01:43 +0200

    test(encoding): add test for partial success and state :failed case

--- test/data_aggregator/records/encoding/workers/encoder_test.exs
+++ test/data_aggregator/records/encoding/workers/encoder_test.exs
@@ -58,6 +58,40 @@ defmodule DataAggregator.Records.Import.Workers.EncoderTest do
       })
     end
 
+    @tag capture_log: true
+    test "succeeds a partial valid record and performs all encodings but sets state to failed", %{
+      correct_record: correct_record
+    } do
+      correct_record = update_record_fixtures!(correct_record, %{loc_country: nil})
+      expect_correct_swiss_species_api_call()
+
+      {:ok, record} = perform_job(Encoder, %{id: correct_record.id})
+
+      assert record.state == :failed
+
+      encoded_record = EncodedRecord.get_by_record!(record.id)
+
+      assert_map_includes(encoded_record, %{
+        tax_class: "Aves",
+        tax_family: "Muscicapidae",
+        tax_genus: "Oenanthe",
+        tax_kingdom: "Animalia",
+        tax_order: "Passeriformes",
+        tax_phylum: "Chordata",
+        tax_scientific_name: "Enantiulus dentigerus (Verhoeff, 1901)",
+        tax_taxon_id: 1_012_187,
+        tax_accepted_name_usage: "Enantiulus dentigerus (Verhoeff, 1901)",
+        tax_taxon_id_ch: 15_311,
+        tax_taxon_rank: "SPECIES",
+        loc_country: nil,
+        loc_municipality: "Bern",
+        loc_state_province: nil,
+        loc_continent: nil,
+        loc_country_code: nil,
+        iucn_redlist_category: "EX"
+      })
+    end
+
     # at the moment there is no failing matchType, we accept all results
     @tag :pending
     test "fails an invalid record to encode", %{invalid_record: invalid_record} do
