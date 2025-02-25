3dbadfbdb48019df12ee74f8fdb3c62b610ead08
Simon <simon_sidler92@hotmail.com>
2025-02-25 12:00:36 +0100

    fix: remove linebreaks of binary values in csv file creation

--- test/data_aggregator/helpers_test.exs
+++ test/data_aggregator/helpers_test.exs
@@ -23,6 +23,7 @@ defmodule DataAggregator.HelpersTest do
   doctest DataAggregator.Records.Encoding.Strategy.ConvertDateHelpers, import: true
   doctest DataAggregator.Records.Encoding.Strategy.GbifTaxonomyStrategy, import: true
   doctest DataAggregator.Taxonomy.Catalogs.SwissSpeciesImporter, import: true
+  doctest DataAggregator.Misc.FlatFileUtils, import: true
 
   setup do
     stub_with(Gbif.RestAPI, Gbif.RestAPIStub)
