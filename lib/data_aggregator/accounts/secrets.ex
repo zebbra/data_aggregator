6a6f9164de421e9476400d5967275597ea3003b1
Claudio Siegenthaler <cloedu@gmail.com>
2025-04-01 17:02:22 +0200

    fix(loggin_failed_signing): log if secret for signing can't be retrieved

--- lib/data_aggregator/accounts/secrets.ex
+++ lib/data_aggregator/accounts/secrets.ex
@@ -2,12 +2,16 @@ defmodule DataAggregator.Accounts.Secrets do
   @moduledoc false
   use AshAuthentication.Secret
 
+  require Logger
+
   def secret_for([:authentication, :tokens, :signing_secret], DataAggregator.Accounts.User, _, _) do
     case Application.fetch_env(:data_aggregator, DataAggregatorWeb.Endpoint) do
       {:ok, endpoint_config} ->
-        Keyword.fetch(endpoint_config, :secret_key_base)
+        secret = Keyword.fetch(endpoint_config, :secret_key_base)
 
       :error ->
+        Logger.info("No secret key base found in endpoint config")
+
         :error
     end
   end
