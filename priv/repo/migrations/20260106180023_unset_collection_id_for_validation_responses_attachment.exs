ca5ba9ee01ca6b36ec5283e751b8fbccea85253e
Claudio Siegenthaler <claudio.siegenthaler@zebbra.ch>
2026-01-07 10:34:46 +0100

    fix: data digitizer needs to be able to create validation requests + small stuff (#1020)
    * fix: data digitizer needs to be able to create validation requests; adjust policy check to get institution from tenant, if changeset isn't available; remove collection_id from validation_response related medias;
    
    * test: create some test for the institution policy check
    
    * fix: set deleted_at to NULL along with the collection_id when cleaning up attachments related to validation_responses
    
    * fix: set deleted_at to NULL along with the collection_id when cleaning up attachments related to validation_responses
    
    * fix: set deleted_at to NULL along with the collection_id when cleaning up attachments related to validation_responses
    
    * fix: set deleted_at to NULL along with the collection_id when cleaning up attachments related to validation_responses

---
+++ priv/repo/migrations/20260106180023_unset_collection_id_for_validation_responses_attachment.exs
@@ -0,0 +1,19 @@
+defmodule DataAggregator.Repo.Migrations.UnsetCollectionIdForValidationResponses do
+  use Ecto.Migration
+
+  def up do
+    # unset collection_id and deleted_at on all attachments of validation responses, to avoid having them deleted
+    # when cleaning up deleted collection medias
+    execute("""
+    UPDATE file_attachments fa
+    SET collection_id = NULL, deleted_at = NULL
+    FROM validation_responses vr
+    JOIN validation_response_2_collections vrc ON vr.id = vrc.validation_response_id
+    WHERE fa.id = vr.attachment_id OR fa.id = vr.error_log_id
+    """)
+  end
+
+  def down do
+    # data might have changed, no rollback is possible. do it manually.
+  end
+end
